19fd3cb1e54ad020c5815fce54873cd780a805b0
AA9-YG <abewajang@gmail.com>
2023-06-07 21:51:43 -0400

    Added files

--- StudentManagement/src/main/java/entity/Student.java
+++ StudentManagement/src/main/java/entity/Student.java
@@ -1,4 +1,83 @@
 package entity;
 
+import jakarta.persistence.*;
+
+@Entity
 public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long studentID;
+
+    private String firstName;
+
+    private String lastName;
+
+    private int age;
+
+    private double gpa;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Teacher teacher;
+
+
+    public Student() {
+
+    }
+
+    public Student(String firstName, String lastName, int age, double gpa, Teacher teacher) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.gpa = gpa;
+        this.teacher = teacher;
+    }
+
+    public Long getStudentID() {
+        return studentID;
+    }
+
+    public void setStudentID(Long studentID) {
+        this.studentID = studentID;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public double getGpa() {
+        return gpa;
+    }
+
+    public void setGpa(double gpa) {
+        this.gpa = gpa;
+    }
+
+    public Teacher getTeacher() {
+        return teacher;
+    }
+
+    public void setTeacher(Teacher teacher) {
+        this.teacher = teacher;
+    }
 }
---
+++ StudentManagement/src/main/java/entity/Teacher.java
@@ -0,0 +1,88 @@
+package entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+public class Teacher {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long teacherID;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String course;
+
+    private double salary;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "teacher")
+    @JsonIgnore
+    private List<Student> students;
+
+
+    public Teacher() {
+
+    }
+
+    public Teacher(String firstName, String lastName, String course, double salary) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.course = course;
+        this.salary = salary;
+    }
+
+    public Long getTeacherID() {
+        return teacherID;
+    }
+
+    public void setTeacherID(Long teacherID) {
+        this.teacherID = teacherID;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getCourse() {
+        return course;
+    }
+
+    public void setCourse(String course) {
+        this.course = course;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public List<Student> getStudents() {
+        return students;
+    }
+
+    public void setStudents(List<Student> students) {
+        this.students = students;
+    }
+}
