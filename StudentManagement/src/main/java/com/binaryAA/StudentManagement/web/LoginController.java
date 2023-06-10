6350c3868264301e4857a8d94cfd29b8dfdf8b8b
AA9-YG <abewajang@gmail.com>
2023-06-09 21:47:01 -0400

    Finished LoginController

--- StudentManagement/src/main/java/com/binaryAA/StudentManagement/web/LoginController.java
+++ StudentManagement/src/main/java/com/binaryAA/StudentManagement/web/LoginController.java
@@ -3,6 +3,7 @@ package com.binaryAA.StudentManagement.web;
 import com.binaryAA.StudentManagement.pojo.AccountCredentials;
 import org.springframework.beans.factory.annotation.Autowired;
 import com.binaryAA.StudentManagement.jwtService.JWTService;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -22,6 +23,10 @@ public class LoginController {
     public ResponseEntity<?> getToken(@RequestBody AccountCredentials credentials) {
         UsernamePasswordAuthenticationToken creds = new UsernamePasswordAuthenticationToken(credentials.getUsername(), credentials.getPassword());
         Authentication auth = authenticationManager.authenticate(creds);
+        String token = jwtService.getToken(auth.getName());
 
+        return ResponseEntity.ok().header("Authorization", "Bearer" + token)
+                .header(HttpHeaders.ACCESS_CONTROL_ALLOW_HEADERS, "AUTHORIZATION")
+                .build();
     }
 }
